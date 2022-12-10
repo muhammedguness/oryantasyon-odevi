7073deb14365d38301fda444b5b4f32471470af8
muhammedguness <111022765+muhammedguness@users.noreply.github.com>
2022-12-10 23:40:13 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String harf;
-        System.out.println("Harfi giriniz: ");
-        harf = input.nextLine();
-        switch (harf){
-            case "A":
-            case "I":
-            case "O":
-            case "U":
-                System.out.println("Kalın Sesli Harf");
-                break;
-            case "E":
-            case "İ":
-            case "Ö":
-            case "Ü":
-                System.out.println("İnce Sesli Harf");
-                break;
-            default:
-                System.out.println("Girdiğiniz harf sessiz harftir.");
+        int[] numbers = new int[]{1, 2, 4, 6, 8, 34, 12, 13, 34};
 
+        Scanner input = new Scanner(System.in);
+        int aranacakSayi;
+        System.out.println("Aranacak sayıyı giriniz: ");
+        aranacakSayi = input.nextInt();
+
+        boolean control = false;
+
+        for (int liste : numbers) {
+            if(liste == aranacakSayi){
+                control = true;
+            }
+        }
+
+        if (control) {
+            System.out.println("Aradığınız sayı listenin içindedir.");
+        }
+        else {
+            System.out.println("Aradığınız sayı listenin içinde bulunmamaktadır.");
         }
     }
 }
